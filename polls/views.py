cde117d62f60d271b8dbaa689e462aecb1610f4b
Karan Goel <karan@goel.im>
2013-08-13 16:41:09 +0530

    Index and detail views added

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,17 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from polls.models import Poll
+
+
 def index(request):
-    return HttpResponse('hello world')
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse('Looking at poll %s' % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse('Looking at results of poll %s' % poll_id)
