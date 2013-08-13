f46f8f94712c7e7537717baa516cb75387bbed67
Karan Goel <karan@goel.im>
2013-08-13 16:22:46 +0530

    Create placeholder views

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hello world')
+
+def detail(request, poll_id):
+    return HttpResponse('Looking at poll %s' % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse('Looking at results of poll %s' % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse('Voting on poll %s' % poll_id)
